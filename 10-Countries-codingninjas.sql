862336ce1a2cdfd63e607915a3902645df13421d
umashankarpatra <umashankar.patra19@gmail.com>
2022-12-04 00:21:34 +0530

    insert script added

---
+++ 10-Countries-codingninjas.sql
@@ -0,0 +1,13 @@
+//Big Countries
+
+
+//create Cinema table
+CREATE TABLE World (
+    name varchar2(10) NOT NULL,
+    continent varchar2(10),
+    area varchar2(10),
+    population varchar2(10),
+    gdp varchar2(10)
+);
+
+select * from Cinema 
